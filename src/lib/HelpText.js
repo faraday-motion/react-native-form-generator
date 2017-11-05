28e95c0b30887a1bcfa5d748b8cda9719530f099
Constantin Colac <constantin.colac@gmail.com>
2017-11-05 11:37:37 +0100

    PR: https://github.com/MichaelCereda/react-native-form-generator/pull/124

--- src/lib/HelpText.js
+++ src/lib/HelpText.js
@@ -3,7 +3,7 @@
 'use strict';
 
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import { View, StyleSheet, Text} from 'react-native';
 
 export class HelpText extends React.Component{
@@ -17,7 +17,7 @@ export class HelpText extends React.Component{
 }
 
 HelpText.propTypes = {
-  text: React.PropTypes.string
+  text: PropTypes.string
 }
 
 
